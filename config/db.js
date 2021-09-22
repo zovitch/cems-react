351e73b6dbacdfa23532b036e2576ed88ebee818
Nico <zovitch@gmail.com>
2021-09-22 16:33:47 +0800

    fix mongoose connect useCreateIndex depreciated

--- config/db.js
+++ config/db.js
@@ -7,6 +7,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
     });
     console.log('MongoDB connected...');
   } catch (err) {
