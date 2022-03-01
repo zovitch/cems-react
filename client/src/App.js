291be00ad5ab3531cbd5c9868af7006358e5affc
zovitch <zovitch@gmail.com>
2022-03-01 14:45:37 +0800

    reduc store api and axios

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,23 @@ import { Landing } from './components/layout/Landing';
 import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route exact path='/' element={<Landing />} />
-      <Route exact path='/register' element={<Register />} />
-      <Route exact path='/login' element={<Login />} />
-    </Routes>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Landing />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/login' element={<Login />} />
+      </Routes>
+    </Router>
+  </Provider>
 );
 
 export default App;
