ea9a40d4546d193bec90a9e6d752d8efbeb87e94
zovitch <zovitch@gmail.com>
2022-03-02 17:00:47 +0800

    Simple Dashboard showing the profile name

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import Avatar from 'react-avatar';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentUser } from '../../actions/user';
 
-const Dashboard = ({ getCurrentUser, auth, user }) => {
+// Check if user hit back button from an unauthenticated state and force reload to update browser cache
+const perfEntries = performance.getEntriesByType('navigation');
+if (perfEntries.length && perfEntries[0].type === 'back_forward') {
+  if (localStorage.getItem('token') === null) {
+    window.location.reload();
+  }
+}
+
+const Dashboard = ({ getCurrentUser, auth: { user } }) => {
   useEffect(() => {
     getCurrentUser();
   }, [getCurrentUser]);
 
-  return <div>Dashboard</div>;
+  return (
+    <section className='container'>
+      <h1 className='large text-primary'>
+        <i className='fas fa-user' /> My Profile{' '}
+      </h1>
+
+      {user ? (
+        <>
+          <div className='lead '>
+            <Avatar
+              name={user.name}
+              round={true}
+              maxInitials='3'
+              textSizeRatio='1.2'
+              size='50'
+            />
+          </div>
+          <div>{user.name}</div>
+        </>
+      ) : (
+        <Avatar name='?' round={true} maxInitials='3' size='50' />
+      )}
+    </section>
+  );
 };
 
 Dashboard.propTypes = {
