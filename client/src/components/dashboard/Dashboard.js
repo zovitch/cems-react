12b642673104d67c79b5f5d3806af31f82ce5f68
zovitch <zovitch@gmail.com>
2022-03-03 14:04:27 +0800

    Dashboard small update

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,4 @@
-import React, { useEffect, Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import Avatar from 'react-avatar';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentUser } from '../../actions/user';
@@ -21,25 +19,8 @@ const Dashboard = ({ getCurrentUser, auth: { user } }) => {
   return (
     <section className='container'>
       <h1 className='large text-primary'>
-        <i className='fas fa-user' /> My Profile{' '}
+        <i class='fa-solid fa-list-check'></i> Dashboard
       </h1>
-
-      {user ? (
-        <>
-          <div className='lead '>
-            <Avatar
-              name={user.name}
-              round={true}
-              maxInitials='3'
-              textSizeRatio='1.2'
-              size='50'
-            />
-          </div>
-          <div>{user.name}</div>
-        </>
-      ) : (
-        <Avatar name='?' round={true} maxInitials='3' size='50' />
-      )}
     </section>
   );
 };
