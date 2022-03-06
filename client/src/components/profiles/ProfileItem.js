138b43e151286b2b8a45132fd00aa1e32b08ea91
Nico <zovitch@gmail.com>
2022-03-06 22:17:56 +0800

    WIP show department owned

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Avatar from 'react-avatar';
 import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
+import { connect } from 'react-redux';
+import { getDepartments } from '../../actions/department';
 import { Link } from 'react-router-dom';
 
-const ProfileItem = ({ user: { _id, name } }) => {
+const ProfileItem = ({
+  getDepartments,
+  department: { departments },
+  user: { _id, name },
+}) => {
+  useEffect(() => {
+    getDepartments();
+  }, [getDepartments]);
+
   return (
     <div className='profile bg-light'>
       <Link to={`/users/${_id}`}>
@@ -18,12 +29,25 @@ const ProfileItem = ({ user: { _id, name } }) => {
       <Link to={`/users/${_id}`}>
         <h2>{name}</h2>
       </Link>
+      <div>
+        {departments
+          .filter((department) => department.owners.name === name)
+          .map((filteredDepartment) => (
+            <li>{filteredDepartment}</li>
+          ))}
+      </div>
     </div>
   );
 };
 
 ProfileItem.propTypes = {
   user: PropTypes.object.isRequired,
+  department: PropTypes.object.isRequired,
+  getDepartments: PropTypes.func.isRequired,
 };
 
-export default ProfileItem;
+const mapStateToProps = (state) => ({
+  department: state.department,
+});
+
+export default connect(mapStateToProps, { getDepartments })(ProfileItem);
