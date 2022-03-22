00d31d0fbaecd6f949f895c0e2d4f7d30bd5b6ec
zovitch <zovitch@gmail.com>
2022-03-22 16:54:54 +0800

    Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentUser } from '../../actions/user';
 
+import DashBoardCard from '../layout/DashBoardCard';
+import AddNew from '../layout/AddNew';
+
 // Check if user hit back button from an unauthenticated state and force reload to update browser cache
 const perfEntries = performance.getEntriesByType('navigation');
 if (perfEntries.length && perfEntries[0].type === 'back_forward') {
@@ -16,11 +19,66 @@ const Dashboard = ({ getCurrentUser, auth: { user } }) => {
     getCurrentUser();
   }, [getCurrentUser]);
 
+  const listOfItemsOnDashboard = [
+    {
+      name: 'manufacturers',
+      nameCN: '制造商',
+      route: 'manufacturers',
+      faLogo: 'fas fa-industry',
+    },
+    {
+      name: 'Failure Codes',
+      route: 'failurecodes',
+      faLogo: 'fas fa-code',
+      color: 'text-failure',
+    },
+    {
+      name: 'Repair Codes',
+      route: 'repaircodes',
+      faLogo: 'fas fa-code',
+      color: 'text-repair',
+    },
+    {
+      name: 'Analysis Codes',
+      route: 'analysiscodes',
+      faLogo: 'fas fa-code',
+      color: 'text-analysis',
+    },
+    {
+      name: 'Categories',
+      nameCN: '类别',
+      route: 'categories',
+      faLogo: 'fas fa-tags',
+    },
+    {
+      name: 'Locations',
+      nameCN: '位置',
+      route: 'locations',
+      faLogo: 'fas fa-location',
+    },
+    {
+      name: 'departments',
+      nameCN: '申请部门',
+      route: 'departments',
+      faLogo: 'fas fa-briefcase',
+    },
+    {
+      name: 'Users',
+      nameCN: '所有者',
+      route: 'departments',
+      faLogo: 'fas fa-users',
+    },
+  ];
   return (
     <section className='container'>
       <h1 className='large text-primary'>
-        <i className='fa-solid fa-list-check'></i> Dashboard
+        <i className='fa-solid fa-list-check'></i> Citel Equipment & Machinery
+        System
       </h1>
+      <div className='cards py-2'>
+        {listOfItemsOnDashboard.length > 1 &&
+          listOfItemsOnDashboard.map((i) => <DashBoardCard item={i} />)}
+      </div>
     </section>
   );
 };
---
+++ client/src/components/layout/DashBoardCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const DashBoardCard = ({ item: { name, nameCN, route, faLogo, color } }) => {
+  return (
+    <Link to={`/${route}`}>
+      <div className='card bg-light'>
+        <h1>
+          <i className={`fas ${faLogo} fa-2xl ${color}`}></i>
+        </h1>
+        <br />
+        <h1 className={color}> {name[0].toUpperCase() + name.substring(1)} </h1>
+        <h1 className={color}> {nameCN} </h1>
+      </div>
+    </Link>
+  );
+};
+
+DashBoardCard.propTypes = {};
+
+export default DashBoardCard;
